dfb5dd682721857ded26e0c9e59ae33afbf1de9e
jesusgabri3l <jesus15202009@gmail.com>
2023-04-21 20:22:39 -0500

    Trying automatic deploy

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,15 @@ const text = ref('Vue3 + TS + Pinia + ESLint + Prettier + Vitest + @testing-libr
   <h1>{{ text }}</h1>
   <HelloWorld msg="Ready to build and test!" />
 </template>
+<styles lang="css">
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+body {
+  background-color: #8e44ad;
+  color: #fff;
+  font-family: 'Poppins', sans-serif;
+}
+h1 {
+  font-size: 32px;
+  font-weight: 700;
+}
+</styles>
